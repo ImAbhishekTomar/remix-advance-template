f9364a57e77f06c08638d8f697d87c4f4d879ef5
Abhishek Tomar <6933841+ImAbhishekTomar@users.noreply.github.com>
2022-02-19 01:23:36 +0530

    removed

--- remix-app/app/hooks/useSubHeaderHeading.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-const useSubHeaderHeading = (heading: string) => {
-  const [data, setData] = useState<string>(heading);
-  return [data];
-};
-
-export default useSubHeaderHeading;
